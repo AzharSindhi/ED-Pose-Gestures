9fabc419614f361beb1e1e16e2bfd5cbf54cbf69
Mathias <mathias.zinnen@fau.de>
2025-10-24 11:06:17 +0200

    fix modelname option

--- scripts/reproduce_oct25/sanity_check.sh
+++ scripts/reproduce_oct25/sanity_check.sh
@@ -105,12 +105,11 @@ torchrun --nproc_per_node=$SLURM_GPUS_ON_NODE --master_port=$CURRENT_PORT main.p
         --edpose_model_path /home/atuin/b268dc/b268dc10/models/EDPose-R50.pth \
         --edpose_finetune_ignore class_embed. \
         --output_dir ${WORK_DIR}/output/ \
-        --options modelname=classifier \
+        --options modelname=edpose \
             num_classes=$N_CLASSES batch_size=$BS epochs=$epoch lr_drop=$LR_DROP \
             lr=$LR weight_decay=$WEIGHT_DECAY lr_backbone=1e-05 num_body_points=17 backbone=resnet50 \
             set_cost_class=2.0 cls_loss_coef=2.0 use_dn=True dn_number=$DN_NUMBER \
             num_queries=$N_QUERIES num_group=$NUM_GROUP \
-            name=edpose \
         --dataset_file=coco --find_unused_params \
         --finetune_edpose \
         --fix_size \
