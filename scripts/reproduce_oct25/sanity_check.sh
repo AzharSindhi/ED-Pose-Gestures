34d464768d1efd242be6a02efd07615f539c72a4
Mathias <mathias.zinnen@fau.de>
2025-10-24 10:52:23 +0200

    add separete-token-per-class to slurm scripts

--- scripts/reproduce_oct25/sanity_check.sh
+++ scripts/reproduce_oct25/sanity_check.sh
@@ -101,6 +101,7 @@ CURRENT_PORT=$((44144+${SLURM_ARRAY_TASK_ID}))
 
 torchrun --nproc_per_node=$SLURM_GPUS_ON_NODE --master_port=$CURRENT_PORT main.py \
         --seperate_classifier --classifier_type full --config_file config/edpose.cfg.py \
+        --seperate-token-per-class \
         --edpose_model_path /home/atuin/b268dc/b268dc10/models/EDPose-R50.pth \
         --edpose_finetune_ignore class_embed. \
         --output_dir ${WORK_DIR}/output/ \
@@ -109,6 +110,7 @@ torchrun --nproc_per_node=$SLURM_GPUS_ON_NODE --master_port=$CURRENT_PORT main.p
             lr=$LR weight_decay=$WEIGHT_DECAY lr_backbone=1e-05 num_body_points=17 backbone=resnet50 \
             set_cost_class=2.0 cls_loss_coef=2.0 use_dn=True dn_number=$DN_NUMBER \
             num_queries=$N_QUERIES num_group=$NUM_GROUP \
+            name=edpose \
         --dataset_file=coco --find_unused_params \
         --finetune_edpose \
         --fix_size \
